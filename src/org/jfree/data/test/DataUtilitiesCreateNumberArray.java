ccee9cd7e5668bb0084ca0dc26901a8cf13dcbe3
Ashley Brown <ashley.brown1@ucalgary.ca>
2019-03-12 13:49:00 -0600

    Update DataUtilitiesCreateNumberArray.java
    Deleting duplicate tests from merging

--- src/org/jfree/data/test/DataUtilitiesCreateNumberArray.java
+++ src/org/jfree/data/test/DataUtilitiesCreateNumberArray.java
@@ -234,30 +234,6 @@ public class DataUtilitiesCreateNumberArray extends DataUtilities {
 	}
 	// @Test(expected = InvalidParameterException.class)
 	
-	/*NEW TESTS*/
-	
-	/*
-	 * Sends null in as argument when creating Number Array
-	 * */
-	@Test(expected=InvalidParameterException.class)
-	public void createNumberArrayWithNull() {
-		DataUtilities.createNumberArray(null);
-	}
-	
-	/*
-	 * Sends null in as argument when creating 2D Number Array
-	 * */
-	@Test(expected=InvalidParameterException.class)
-	public void create2DNumberArrayWithNull() {
-		DataUtilities.createNumberArray2D(null);
-	}
-	
-	/*
-	 * Sends null in as argument when getting cumulative percentages
-	 * */
-	@Test(expected=InvalidParameterException.class)
-	public void getCumulativePercentagesWithNull() {
-		DataUtilities.getCumulativePercentages(null);
-	}
+
 	
 }
