f8d737417c235254e135f38a34bef2f550668cc9
Ashley Brown <ashley.brown1@ucalgary.ca>
2019-03-12 13:50:00 -0600

    Update RangeTest.java
    Deleting more duplicates

--- src/org/jfree/data/test/RangeTest.java
+++ src/org/jfree/data/test/RangeTest.java
@@ -235,41 +235,6 @@ public class RangeTest {
 	public static void tearDownAfterClass() throws Exception {
 	}
 	
-	/*NEW TESTS*/
-	
-	/*
-	 * Creates Range with lower bound larger than upper
-	 * */
-	@Test(expected=IllegalArgumentException.class)
-	public void rangeWithLowerGreaterThanUpper() {
-		double lower = 10;
-		double upper = 0;
-		new Range(lower, upper);	
-	}
-	
-	/*
-	 * Test Constrain when value isnt in range - above range
-	 * */
-	@Test
-	public void constrainValueAboveRange() {
-		double lower = -10;
-		double upper = 10;
-		Range exampleRange = new Range(lower, upper);
-		double result = exampleRange.constrain(upper + 1);
-		assertEquals("Constrain upper when value out of range", upper, result, .000000001d);
-	}
-	
-	/*
-	 * Test Constrain when value isnt in range - below range
-	 * */
-	@Test
-	public void constrainValueBelowRange() {
-		double lower = -10;
-		double upper = 10;
-		Range exampleRange = new Range(lower, upper);
-		double result = exampleRange.constrain(lower - 1);
-		assertEquals("Constrain lower when value out of range", lower, result, .000000001d);
-	}
-	
+
 	
 }
